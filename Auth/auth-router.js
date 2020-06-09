1388252815450b05298c36b4b17d6efa6577c964
stevenoakley211 <stevenoakley211@live.com>
2020-06-08 20:54:38 -0500

    almost mvp

--- Auth/auth-router.js
+++ Auth/auth-router.js
@@ -2,16 +2,18 @@ const router = require('express').Router();
 const Users = require('../users/users-model.js')
 const bcrypt =require('bcrypt')
 const isValid = require('../Middleware/users-service')
+const jwt = require( 'jsonwebtoken')
 router.post('/register', (req, res) =>{
     let user = req.body;
     const hash = bcrypt.hashSync(user.password,10)
-
-    if(isValid(req.body))
     Users.add({
         username: user.username,
         password: hash
     })
-    .then(res => res.status(200).json(res))
+    .then(newUser =>{
+        const token = genToken(newUser)
+        res.status(201).json({created_user: newUser,token:token})
+    })
     .catch(err => res.status(500).json({message:"something went wrong",error:err}))
 })
 
